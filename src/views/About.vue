261d44fe20d5b2b66040a661f9ee910bef8140fb
LARE <zuoyun-135@163.com>
2021-03-21 17:05:22 +0800

    save before dinner

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,21 +1,64 @@
 <template>
   <div class="about">
-    <router-link to="/"> 
-    Back </router-link>
-    <h1>This is an about page</h1>
+    <div class="about-box">
+      <router-link class="back" to="/">
+        Back
+      </router-link>
+      <h1>TWT Editor</h1>
+      <p>Version 0.1.0</p>
+      <p>
+        Made with Vue3.x + Electron
+      </p>
+      <div class="icons">
+        
+      </div>
+    </div>
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue'
+import { defineComponent } from "vue";
+
 export default defineComponent({
-  setup() {
-    
-  },
-})
+  setup() {},
+});
 </script>
 
 <style lang="scss" scoped>
-
+.about {
+  height: 100%;
+  background: #fff;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  .about-box {
+    position: relative;
+    background: #2f3241;
+    width: 700px;
+    height: 470px;
+    border-radius: 10px;
+    color: #74b1be;
+    .back {
+      position: absolute;
+      top: 10px;
+      left: 10px;
+      text-decoration: none;
+      font-weight: bold;
+      color: #fff;
+    }
+    h1 {
+      text-align: center;
+      margin-top: 30px;
+    }
+    p {
+      text-align: center;
+      margin-top: 10px;
+    }
+    .icons {
+      width: 100%;
+      display: flex;
+      justify-content: space-around;
+    }
+  }
+}
 </style>
-
