74a7d7fcb3c79695b40d0bfc9bec4b132eceb143
LARE <zuoyun-135@163.com>
2021-03-21 18:25:49 +0800

    about

--- src/views/About.vue
+++ src/views/About.vue
@@ -4,13 +4,18 @@
       <router-link class="back" to="/">
         Back
       </router-link>
+      <div class="logo-box">
+        <img class="logo" :src="twtLogo" />
+      </div>
       <h1>TWT Editor</h1>
       <p>Version 0.1.0</p>
       <p>
         Made with Vue3.x + Electron
       </p>
       <div class="icons">
-        
+        <img :src="vueLogo" />
+        +
+        <img :src="electronLogo" />
       </div>
     </div>
   </div>
@@ -20,7 +25,12 @@
 import { defineComponent } from "vue";
 
 export default defineComponent({
-  setup() {},
+  setup() {
+    const vueLogo = require("@/assets/vue.png");
+    const electronLogo = require("@/assets/electron.png");
+    const twtLogo = require("@/assets/logo.png");
+    return { vueLogo, electronLogo, twtLogo };
+  },
 });
 </script>
 
@@ -40,24 +50,41 @@ export default defineComponent({
     color: #74b1be;
     .back {
       position: absolute;
-      top: 10px;
-      left: 10px;
+      top: 20px;
+      left: 20px;
+      line-height: 20px;
       text-decoration: none;
       font-weight: bold;
       color: #fff;
     }
     h1 {
       text-align: center;
-      margin-top: 30px;
+      margin-top: 20px;
     }
     p {
       text-align: center;
       margin-top: 10px;
     }
     .icons {
-      width: 100%;
+      margin-top: 20px;
+      line-height: 40px;
+      font-size: 24px;
       display: flex;
-      justify-content: space-around;
+      justify-content: center;
+    }
+    img {
+      width: 40px;
+      height: 40px;
+      margin: 0 20px;
+    }
+    .logo-box {
+      display: flex;
+      justify-content: center;
+      .logo {
+        width: 120px;
+        height: 120px;
+        margin-top: 100px;
+      }
     }
   }
 }
